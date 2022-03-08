71b360dbeb97f148a546f76943abfc8a848b0a3c
Ashlin Iser <markus.iser@kit.edu>
2022-03-08 08:22:22 +0100

    improved build script of solbert

--- solbert/install-pymod.sh
+++ solbert/install-pymod.sh
@@ -1,3 +1,6 @@
+mkdir build
+cmake -B build -S .
+cmake --build build
 rm -rf build/lib.linux-x86_64-3.8/ build/temp.linux-x86_64-3.8/
 python3 setup.py build
 python3 setup.py install --record uninstall.info --force
