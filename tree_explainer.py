a50f5723c9d69d1503ed290b04470813ad318b46
Markus Iser <markus.iser@kit.edu>
2022-03-15 17:34:02 +0100

    cleanups

--- tree_explainer.py
+++ tree_explainer.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 # Determine Prime Implicants of Random Forest Classifiers
 # Copyright (C) 2022 Markus Iser, Karlsruhe Institute of Technology (KIT)
 # 
@@ -15,187 +18,109 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
+import pandas as pd
+from sklearn import tree, ensemble
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
+from gbd_tool.gbd_api import GBD
+from gbd_tool.util import eprint
+
+from tree_encoder import DecisionTreeEncoder
 from tree_wrapper import DecisionTreeWrapper
-
-from solbert import compute_prime_implicants
-
-
-class VariableProducer:
-    def __init__(self):
-        self.vars = 0
-
-    def new_var(self):
-        self.vars = self.vars + 1
-        return self.vars
 
 
 class DecisionTreeExplainer:
 
-    def __init__(self, tree: DecisionTreeWrapper, vprod: VariableProducer = None):
-        self.dtw = tree
-        self.vprod = vprod if vprod != None else VariableProducer()
-        self.vars = 0
-        # class variables:
-        self.vclasses = [ self.new_var() for _ in range(self.dtw.n_classes()) ]
-        # node variables:
-        self.vnodestrue = [ self.new_var() for _ in range(self.dtw.n_nodes()) ]
-        self.vnodesfalse = [ self.new_var() for _ in range(self.dtw.n_nodes()) ]
-        # value variables:
-        self.vintervals = []
-        for feat_id in range(self.dtw.n_features()):
-            self.vintervals.append([ self.new_var() for _ in self.dtw.feature_values(feat_id) ])
-        self.vintervall = []
-        for feat_id in range(self.dtw.n_features()):
-            self.vintervall.extend(self.vintervals[feat_id])
-        self.clauses = self.encode()
+    def __init__(self, query, api: GBD, wrapper: DecisionTreeWrapper):
+        self.query = query
+        self.api = api
+        self.wrapper = wrapper
+        self.encoder = DecisionTreeEncoder(wrapper)
+        self.cats = self.wrapper.class_names
+        self.implicants = dict()
+        for cat in self.cats:
+            eprint("Calculating prime implicants for category {}".format(str(cat)))
+            self.implicants[cat] = self.encoder.explain([cat])
+            self.implicants[cat].sort(key=len)
 
 
-    def new_var(self):
-        return self.vprod.new_var()
+    def print_implicants(self):
+        cat_leafs = []
+        cat_imps = []
+        for cat in self.cats:
+            (leafs, imps) = self.explain(cat)
+            cat_leafs.append(leafs)
+            cat_imps.append(imps)
+        self.plot(cat_leafs, cat_imps)
 
 
-    def class2var(self, class_id):
-        return self.vclasses[class_id]
-
-    def var2class(self, var_id):
-        return self.vclasses.index(var_id)
-
-
-    def node2var(self, node_id: int, tip: bool):
-        return self.vnodestrue[node_id] if tip else self.vnodesfalse[node_id]
-
-    def var2node(self, var_id, tip: bool):
-        return self.vnodestrue.index(var_id) if tip else self.vnodesfalse.index(var_id)        
-
-
-    def feat2vars(self, feat_id):
-        return self.vintervals[feat_id]
-
-    def var2val(self, var_id):
-        for feat_id in range(self.dtw.n_features()):
-            if var_id in self.vintervals[feat_id]:
-                return (feat_id, self.vintervals[feat_id].index(var_id))
-        assert False, "variable {} not found".format(var_id)
-
-
-    def explain(self, targetclasses):
-        targetclause = [ self.class2var(self.dtw.class_id(name)) for name in targetclasses ]
-
-        return compute_prime_implicants(self.clauses + [ targetclause ], self.vintervall)
-
-
-    def decode(self, implicant):
-        query = []
-        nfeats = 0
-        ncases = 0
-        for feat_id in range(self.dtw.n_features()):
-            feat = self.dtw.feature_name(feat_id)
-            prev = ncases
-            for i in range(len(self.vintervals[feat_id])-1):
-                m0 = -self.vintervals[feat_id][i] in implicant
-                m1 = -self.vintervals[feat_id][i+1] in implicant
-                if m0 != m1:
-                    ncases = ncases + 1
-                    thre = self.dtw.feature_value(feat_id, i)
-                    form = "{:7f}".format(thre).rstrip('0').rstrip('.')
-                    query.append("{} {} {}".format(feat, ">" if m0 else "<=", form))
-            if prev != ncases:
-                nfeats = nfeats + 1
-        result = { "features": nfeats, "cases": ncases }
-        if len(query) > 10:
-            split = int(len(query)/2)
-            query1 = " and ".join(query[:split])
-            query2 = " and ".join(query[split:])
-            result["query"] = "(" + query1 + ") and (" + query2 + ")"
-        else:
-            result["query"] = " and ".join(query)
-        return result
+    def explain(self, cat):
+        imps = self.implicants[cat]
+        eprint("-" * 42)
+        eprint("Explaining category: {}".format(cat))
+        leafs = self.wrapper.leaf_nodes(cat)
+        eprint("Number of Leaf Nodes: {}".format(len(leafs)))
+        eprint("Number of Prime Implicants: {}".format(len(imps)))
+        cases = []
+        for i, imp in enumerate(imps):
+            explanation = self.encoder.decode(imp)
+            cases.append(explanation["cases"])
+        eprint("Leaf Depths:                 {}".format(str(sorted([ self.wrapper.node_depth(leaf) for leaf in leafs ]))))
+        eprint("Implicant Case Distinctions: {}".format(str(sorted(cases))))
+        # leaf depths and sample numbers
+        L = []
+        for leaf in leafs:
+            depth = self.wrapper.node_depth(leaf)
+            samples = self.wrapper.node_samples_total(leaf)
+            L.append((depth, samples))
+        L.sort(key = lambda x: x[1], reverse=True)
+        # implicant size and sample numbers
+        I = []
+        for i, imp in enumerate(imps):
+            explanation = self.encoder.decode(imp)
+            hashes = self.api.query_search(self.query + " and " + explanation["query"])
+            size = explanation["features"]
+            samples = len(hashes)
+            I.append((size, samples))
+        I.sort(key = lambda x: x[1], reverse=True)
+        print("Leaf Depths and Samples: " + str(L))
+        print("Implicant Size and Samples: " + str(I))
+        return (L, I)
 
 
-    def encode(self):
-        class_constraints = self.encode_class_constraints()
-        node_constraints = self.encode_node_constraints()
-        value_constraints = self.encode_value_constraints()
-        return class_constraints + node_constraints + value_constraints
+    def plot(self, leaf_data, imp_data):
+        from matplotlib import pyplot as plt
+        from statistics import mean
+        sizes = []
+        ncd_ratios = []
+        for i in range(len(self.cats)):
+            L = leaf_data[i]
+            I = imp_data[i]
+            ncd_leafs = [ leaf[0] for leaf in L ]
+            cov_leafs = [ leaf[1] for leaf in L ]
+            ncd_imps = [ imp[0] for imp in I ]
+            sizes.append(sum(cov_leafs))
+            ncd_ratios.append(mean(ncd_imps) / mean(ncd_leafs))
+        fig, ax = plt.subplots()
+        ax.set_xlabel("Family Size (Number of Samples)")
+        ax.set_ylabel("NCD Ratio")
+        plt.title("NCD Ratio vs. Family Size")
+        plt.scatter(sizes, ncd_ratios, marker='x')
+        plt.legend(loc='upper right')
+        plt.show()
 
-
-    # class implies leafs
-    def encode_class_constraints(self):
-        clauses = [ [ -v ] for v in self.vclasses ]
-        for node in range(self.dtw.n_nodes()):
-            if not self.dtw.is_inner_node(node):
-                class_id = self.dtw.node_class(node)
-                clauses[class_id].append(self.node2var(node, True))
-        return clauses
-
-
-    # child implies parent
-    def encode_node_constraints(self):
-        clauses = []
-        for node in range(self.dtw.n_nodes()):
-            left = self.dtw.left_child(node)
-            right = self.dtw.right_child(node)
-            if self.dtw.is_inner_node(node):
-                clauses.append([ -self.node2var(left, True), self.node2var(node, True) ])
-                clauses.append([ -self.node2var(left, False), self.node2var(node, True) ])
-                clauses.append([ -self.node2var(right, True), self.node2var(node, False) ])
-                clauses.append([ -self.node2var(right, False), self.node2var(node, False) ])
-        return clauses
-
-
-    # node disables values
-    def encode_value_constraints(self):
-        clauses = []
-        for node in range(self.dtw.n_nodes()):
-            if self.dtw.is_inner_node(node):
-                feat = self.dtw.node_feature(node)
-                thre = self.dtw.node_threshold(node)
-                split = 1+self.dtw.feature_values(feat).index(thre)
-                vleft = self.vintervals[feat][:split]
-                vright = self.vintervals[feat][split:]
-                for v in vleft: # false disables left values:
-                    clauses.append([ -self.node2var(node, False), v ])
-                for v in vright: # true disables right values:
-                    clauses.append([ -self.node2var(node, True), v ])
-        return clauses
-
-
-    def print(self, model, ranges=True, nodes=False, classes=False):
-        if classes:
-            for v in self.vclasses:
-                if model[v-1] > 0:
-                    class_id = self.var2class(v)
-                    print("Class: " + self.dtw.class_name(class_id))
-        if nodes:
-            for v in self.vnodestrue:
-                if model[v-1] > 0:
-                    node = self.var2node(v, True)
-                    feat = self.dtw.node_feature_name(node)
-                    thre = self.dtw.node_threshold(node)
-                    print("Node {id}: {f} <= {t}".format(id=node, f=feat, t=thre))
-            for v in self.vnodesfalse:
-                if model[v-1] > 0:
-                    node = self.var2node(v, False)
-                    feat = self.dtw.node_feature_name(node)
-                    thre = self.dtw.node_threshold(node)
-                    print("Node {id}: {f} > {t}".format(id=node, f=feat, t=thre))
-        if ranges:
-            for feat_id in range(self.dtw.n_features()):
-                feat = self.dtw.feature_name(feat_id)
-                if not all(model[v-1] < 0 for v in self.vintervals[feat_id]):
-                    value_ranges = [ [-np.infty, np.infty] ]
-                    for i in range(len(self.vintervals[feat_id])-1):
-                        m0 = self.sat(model, self.vintervals[feat_id][i])
-                        m1 = self.sat(model, self.vintervals[feat_id][i+1])
-                        if m0 and not m1:
-                            if value_ranges[-1][0] != -np.infty:
-                                value_ranges.append([-np.infty, np.infty])
-                            value_ranges[-1][0] = self.dtw.feature_value(feat_id, i)
-                        elif not m0 and m1:
-                            if value_ranges[-1][1] != np.infty:
-                                value_ranges.append([-np.infty, np.infty])
-                            value_ranges[-1][1] = self.dtw.feature_value(feat_id, i)
-                    print("{} in {}".format(feat, ", ".join([ "({}, {}]".format(v[0], v[1]) for v in value_ranges ])))
-
-
+        for i in range(len(self.cats)):
+            L = leaf_data[i]
+            I = imp_data[i]
+            cat = self.cats[i]
+            ncd_leafs = [ leaf[0] for leaf in L ]
+            ncd_imps = [ imp[0] for imp in I ]
+            if sizes[i] > 0:#350:
+                K = ["NCD (Leafs)", "NCD (Implicants)"]
+                df = pd.concat([pd.Series(ncd_leafs), pd.Series(ncd_imps)], keys=K, ignore_index=True, axis=1)
+                print(df)
+                plt.title("Family: {}, NCD Ratio: {:2f}".format(cat.upper(), mean(ncd_imps) / mean(ncd_leafs)))
+                plt.boxplot(df)
+                plt.xticks(range(1, len(K)+1), K)
+                plt.show()
